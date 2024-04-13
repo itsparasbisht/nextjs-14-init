d21882bca9e868ad20bb3fd39ee94e3254174a23
itsparsbisht <iamparasbisht@gmail.com>
2024-04-13 20:27:34 +0530

    cookies on route

--- app/blog/api/[id]/route.ts
+++ app/blog/api/[id]/route.ts
@@ -1,11 +1,17 @@
-export async function GET(
-  req: Request,
-  { params }: { params: { id: string } }
-) {
-  console.log(req.headers.get("Authorization"));
+type Context = {
+  params: {
+    id: string;
+  };
+};
+
+export async function GET(req: Request, { params }: Context) {
+  const cookie = req.headers.get("cookie");
+  console.log(cookie);
+
   return new Response(params.id, {
     headers: {
       "content-type": "application/json",
+      "set-cookie": "theme=light",
     },
   });
 }
